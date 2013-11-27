6d2b5cb6e5978cce2711b5ea14f5f73afa874c4e
Cameron Daigle <camerondaigle@gmail.com>
2013-11-27 16:21:29 -0500

    Add middleman-deploy

--- config.rb
+++ config.rb
@@ -3,6 +3,11 @@ activate :livereload
 activate :syntax
 set :haml, { ugly: true }
 
-(1..7).to_a.each do |i|
+(1..4).to_a.each do |i|
   proxy "/examples/#{i}", "/examples/index.html", :locals => { :example => i }, ignore: true
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
 end
