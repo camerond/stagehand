d3e71bd6a7ab113d129a95491c30351da9425611
Cameron Daigle <camerondaigle@gmail.com>
2013-11-27 15:31:52 -0500

    Enable syntax highlighting

--- config.rb
+++ config.rb
@@ -1,2 +1,8 @@
 set :js_dir, 'javascripts'
 activate :livereload
+activate :syntax
+set :haml, { ugly: true }
+
+(1..7).to_a.each do |i|
+  proxy "/examples/#{i}", "/examples/index.html", :locals => { :example => i }, ignore: true
+end
